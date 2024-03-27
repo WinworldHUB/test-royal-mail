aa30a1ab13f4935a90fc668c48c1c24fe0550099
Amit Dubey <adubey@cynergybank.co.uk>
2024-03-27 20:22:40 +0000

    Updated

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", async (req, res) => {
-  const response = await fetch("https://www.royalmail.com");
+  const response = await fetch(
+    "https://www.royalmail.com/track-your-item#/tracking-results/WG339979238GB"
+  );
   const body = await response.text();
   res.send(body);
 });
